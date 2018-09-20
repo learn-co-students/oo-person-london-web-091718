c7621622f558cd30b324f79a24af06d9b7e68dc7
Amalie Smidth <amaliesmidth@gmail.com>
2018-09-20 09:41:23 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -22,5 +22,65 @@ attr_reader :name, :happiness, :hygiene
     @hygiene = 0 if @hygiene < 0
   end
 
+  def happy?
+    if happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def clean?
+    if hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene += 4
+    # hygiene=(self.hygiene)
+    '♪ Rub-a-dub just relaxing in the tub ♫'
+  end
+
+  def work_out
+    self.hygiene -= 3
+    # hygiene=(self.hygiene)
+    self.happiness += 2
+    # happiness=(self.happiness)
+    '♪ another one bites the dust ♫'
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    # happiness=(self.happiness)
+    # happiness=(friend.happiness)
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == 'politics'
+      self.happiness -= 2
+      person.happiness -= 2
+      # happiness=(self.happiness)
+      # happiness=(friend.happiness)
+      'blah blah partisan blah lobbyist'
+    elsif topic == 'weather'
+      self.happiness += 1
+      person.happiness += 1
+      # happiness=(self.happiness)
+      # happiness=(friend.happiness)
+      'blah blah sun blah rain'
+    else
+      'blah blah blah blah blah'
+    end
+  end
 
 end
